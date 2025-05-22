d603046b0b1199850f29c0badb9b77726f800d5b
billy.berkouwer <billy@ocove.studio>
2025-05-22 17:10:58 +0100

    update: websocket connection with server to listen for streamInfo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
 import "./style.css";
+import { io } from "socket.io-client";
+
+const socket = io(import.meta.env.SERVER_ORIGIN);
 
 const playerOne = document.querySelector("#mux-player") as HTMLVideoElement;
 const playerOneCover = document.querySelector("#mux-error__cover-1")
 
-playerOne?.addEventListener("error", (e) => {
-  playerOneCover?.classList.add("visible")
-  setInterval(() => {
-    playerOne?.play();
-  }, 2000)
-});
+let playbackId = import.meta.env.MUX_PLAYBACK_ID;
 
-playerOne?.addEventListener("playing", () => {
-  playerOneCover?.classList.remove("visible")
+function initialiseStream(isStreamActive: boolean) {
+  if (isStreamActive && playerOne) {
+    playerOne.setAttribute("playback-id", playbackId);
+    playerOneCover?.classList.remove("visible")
+  } else {
+    playerOne.setAttribute("playback-id", "");
+    playerOneCover?.classList.add("visible")
+  }
+}
+
+playerOne.addEventListener("canplay", () => playerOne?.play())
+
+socket.on("streamInfo", (e) => {
+  console.log(e)
+  playbackId = e.playbackId;
+  initialiseStream(e.canPlay)
 })
 
-playerOne?.addEventListener("canplay", (e) => {
-  playerOneCover?.classList.remove("visible")
-  setInterval(() => {
-    playerOne?.play();
-  }, 2000)
-  console.log(e, "canplay")
-})
-
+socket.on("isStreamActive", (e) => {
+  const isStreamActive = e;
+  console.log("isStreamActive: ", isStreamActive)
+  initialiseStream(isStreamActive)
+}
+)
