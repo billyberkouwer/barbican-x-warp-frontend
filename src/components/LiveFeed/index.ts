fdb1f5d920a0eb59630b05747a329470df75dac6
billy.berkouwer <billy@ocove.studio>
2025-05-27 12:15:11 +0100

    update: get jwt from server

--- src/components/LiveFeed/index.ts
+++ src/components/LiveFeed/index.ts
@@ -1,12 +1,20 @@
 import { io } from "socket.io-client";
 import { DEFAULT_PLAYBACK_ID, SERVER_ORIGIN } from "../../variables/constants";
 
-export default function initLiveFeed() {
+export default async function initLiveFeed() {
     const socket = io(SERVER_ORIGIN);
     let playbackId = DEFAULT_PLAYBACK_ID;
 
     const playerOne = document.querySelector("#mux-player") as HTMLVideoElement;
     const playerOneCover = document.querySelector("#mux-error__cover-1")
+
+    await fetch(process.env.VITE_SERVER_ORIGIN + "/sign-in", {
+        method: "GET",
+    }).then(async res => {
+        const json = await res.json();
+        console.log(json.token)
+        playerOne.setAttribute("playback-token", json.token);
+    }).catch((err) => console.log(err))
 
     function initialiseStream(isStreamActive: boolean) {
         if (isStreamActive && playerOne) {
