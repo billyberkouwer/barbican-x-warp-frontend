5113cc06c64126eede0ee7f47c0e4ba5054cd5b5
billy.berkouwer <billy@ocove.studio>
2025-05-27 14:22:46 +0100

    fix: bypass ngrok warning

--- src/components/LiveFeed/index.ts
+++ src/components/LiveFeed/index.ts
@@ -10,11 +10,12 @@ export default async function initLiveFeed() {
 
     await fetch(SERVER_ORIGIN + "/sign-in", {
         method: "GET",
+        headers: { "ngrok-skip-browser-warning": "69420" }
     }).then(async res => {
-        const text = await res.text();
-        console.log(text)
-        // console.log({ "customToken": json.token.length, "dashboardToken": "eyJhbGciOiJSUzI1NiJ9.eyJraWQiOiJadnpLdGlyeGhyYkNrdTVTY0ZHVjdTWlp1WndOYUlVeUg2N25NcTRKTVlZIiwiYXVkIjoidiIsInN1YiI6InZhQU1pc0FHbW82TUhOWHMzd2pWREY5VEtaZnBZWXRJb3BsUUl2bTAxNmwwMCIsImV4cCI6MTc0ODQzNTAwOX0.deDp38b4fmZyauK9fHmWPONcspNx0ko71Na6PAdScodyfWrIMylWk-Sl8P2-vatxV-MAfE4aSza8wds9ofS_IK5fJ2bRqh3C6Jos1DFXiSTDhvUG898F75c3aAYmJFKBbEZOmv9fGtkgYc7mAYtYrgHzC46KDj1xKRB6NNTHQ1Xcbui15QI35SDLInciEBnkT55H5vmsmXkfRP-S0Eo7S1fDnVeDuQIc_PoM2Mo42yralvT5V5dWf_iz53ZkQxQ5yxS9c5qJRli3P_si-4FG984fb0UFTV4XmaMcfi8QGKPiHWWRNAgqmydKCZB9GtbtUrkokW9neKuG0TBeBXW5rA".length })
-        // playerOne.setAttribute("playback-token", json.token);
+        const json = await res.json();
+        console.log(json)
+        console.log({ "customToken": json.token.length, "dashboardToken": "eyJhbGciOiJSUzI1NiJ9.eyJraWQiOiJadnpLdGlyeGhyYkNrdTVTY0ZHVjdTWlp1WndOYUlVeUg2N25NcTRKTVlZIiwiYXVkIjoidiIsInN1YiI6InZhQU1pc0FHbW82TUhOWHMzd2pWREY5VEtaZnBZWXRJb3BsUUl2bTAxNmwwMCIsImV4cCI6MTc0ODQzNTAwOX0.deDp38b4fmZyauK9fHmWPONcspNx0ko71Na6PAdScodyfWrIMylWk-Sl8P2-vatxV-MAfE4aSza8wds9ofS_IK5fJ2bRqh3C6Jos1DFXiSTDhvUG898F75c3aAYmJFKBbEZOmv9fGtkgYc7mAYtYrgHzC46KDj1xKRB6NNTHQ1Xcbui15QI35SDLInciEBnkT55H5vmsmXkfRP-S0Eo7S1fDnVeDuQIc_PoM2Mo42yralvT5V5dWf_iz53ZkQxQ5yxS9c5qJRli3P_si-4FG984fb0UFTV4XmaMcfi8QGKPiHWWRNAgqmydKCZB9GtbtUrkokW9neKuG0TBeBXW5rA".length })
+        playerOne.setAttribute("playback-token", json.token);
     }).catch((err) => console.log(err))
 
     function initialiseStream(isStreamActive: boolean) {
