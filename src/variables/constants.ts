eab343bc19f9b77b2ffea347d5b4edfa158546d2
billy.berkouwer <billy@ocove.studio>
2025-05-28 15:11:32 +0100

    tweak: terminal window type speed

--- src/variables/constants.ts
+++ src/variables/constants.ts
@@ -1,4 +1,4 @@
-export const DATA_FEED_TYPE_SPEED = 1000;
-export const DATA_FEED_NEW_LINE_SPEED = 1;
+export const DATA_FEED_TYPE_SPEED = 100;
+export const DATA_FEED_NEW_LINE_SPEED = 10000;
 export const DEFAULT_PLAYBACK_ID = import.meta.env.VITE_DEFAULT_MUX_PLAYBACK_ID;
 export const SERVER_ORIGIN = import.meta.env.VITE_SERVER_ORIGIN;
