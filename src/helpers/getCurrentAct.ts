a51eebf3e3bbcfd8cc5737dae8b5f7905979756e
billy.berkouwer <billy@ocove.studio>
2025-06-03 18:35:22 +0100

    test: log json

--- src/helpers/getCurrentAct.ts
+++ src/helpers/getCurrentAct.ts
@@ -5,5 +5,6 @@ import { EVENT_DATE } from "@/variables/constants";
 const allActs = [...json.act_1, ...json.act_2, ...json.act_3];
 
 export default function getCurrentAct() {
+    console.log(json)
     return allActs.filter(act => getEventProgress(new Date(EVENT_DATE + act.startTime), new Date(EVENT_DATE + act.endTime)).inProgress)[0];
 }
