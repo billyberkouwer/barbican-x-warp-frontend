d8ced5a31c4a7700f65edcb9d7af18502f077b55
billy.berkouwer <billy@ocove.studio>
2025-06-04 17:50:52 +0100

    fix: add svg assets to public folder so dynamically added svgs are accessible in production

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
+  assetsInclude: ["/src/components/SVGs/**.svg"],
   css: {
     preprocessorOptions: {
       scss: {
